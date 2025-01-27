d5cdc7ccf8107a3103021c637db5a0e614148bd6
Daehyun Kim <kimdhyungg@gmail.com>
2025-01-27 20:02:12 +0900

    feat(main): filter out instead of validation

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,25 @@ fn main() -> Result<()> {
     let log_file = &args[1];
     let content = fs::read_to_string(log_file)?;
 
-    let entries = parse_log_entries(&content);
-    debug_assert!(
-        LogEntry::validate_entries(&entries).is_ok(),
-        "Validation failed"
-    );
+    println!("Parsing log entries...");
+    let entries = parse_log_entries(&content)?;
+
+    // debug_assert!(LogEntry::validate_entries(&entries).is_ok());
+    // LogEntry::validate_entries(&entries)?; // instead of validate, I just filter out
+
+    println!("Filtering out invalid entries...");
+    let entries: Vec<LogEntry> = entries
+        .into_iter()
+        .filter(|entry| {
+            let query_no_of_placeholders = entry.query.bytes().filter(|&b| b == b'?').count();
+            query_no_of_placeholders == entry.bind_statements.len()
+        })
+        .collect();
 
     let mut db = Database::new("queries.db")?;
     db.initialize()?;
+
+    println!("Processing log entries...");
     db.process_entries(&entries)?;
 
     Ok(())
